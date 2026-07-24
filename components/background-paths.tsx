61c90ecdcbd3f684d9252da0064430fca0d9388a
ky17824 <kyeon7@gmail.com>
2026-07-24 12:37:03 +0900

    Reduce landing path rendering load

--- components/background-paths.tsx
+++ components/background-paths.tsx
@@ -1,17 +1,12 @@
 function FloatingPaths({ position }: { position: number }) {
   return (
     <svg viewBox="0 0 696 316" preserveAspectRatio="none">
-      {Array.from({ length: 24 }, (_, index) => (
+      {Array.from({ length: 12 }, (_, index) => (
         <path
           key={index}
           d={`M-${380 - index * 5 * position} -${189 + index * 6}C-${380 - index * 5 * position} -${189 + index * 6} -${312 - index * 5 * position} ${216 - index * 6} ${152 - index * 5 * position} ${343 - index * 6}C${616 - index * 5 * position} ${470 - index * 6} ${684 - index * 5 * position} ${875 - index * 6} ${684 - index * 5 * position} ${875 - index * 6}`}
-          pathLength="1"
-          strokeWidth={0.5 + index * 0.04}
-          strokeOpacity={0.08 + index * 0.018}
-          style={{
-            animationDelay: `${index * -0.7}s`,
-            animationDuration: `${22 + (index % 7)}s`
-          }}
+          strokeWidth={0.6 + index * 0.06}
+          strokeOpacity={0.12 + index * 0.035}
         />
       ))}
     </svg>
